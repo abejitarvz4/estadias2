97daaadb8e613184738af02e5c77194f26064e26
abejitarvz4 <109394459+abejitarvz4@users.noreply.github.com>
2024-08-13 12:33:07 -0700

    inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="navbar">
+        <a href="/" class="active">Inicio</a>
+        <a href="../graficas.jsx">Graficas</a>
+        <a href="../productos.jsx">Productos</a>
+    </div>
+      <div container>
+        hola mundo, esta es mi primer pagina sin saber nada 
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
